c55f3b8be97c1d1e0bc986e329ffba5634eb28f0
Becca Little <beccannlittle@gmail.com>
2018-06-11 20:05:23 -0700

    added mobile art to Space Shooter. Got through section 5 of Survival Shooter

---
+++ Survival_Shooter/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLength = 100f;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        floorMask = LayerMask.GetMask("Floor");
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Moving(h, v);
+        Turning();
+        Animating(h, v);
+    }
+
+    private void Moving(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    private void Turning() 
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+    private void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+        anim.SetBool("IsWalking", walking);
+    }
+
+}

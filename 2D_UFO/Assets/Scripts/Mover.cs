9f18be075684fc656d5e1b031c1b323a8f4f6152
Becca Little <beccannlittle@gmail.com>
2018-07-06 10:15:57 -0700

    made sure I have two separate versions of 2D UFO saved. finished first three chapters of Procedural Cave Generation

---
+++ 2D_UFO/Assets/Scripts/Mover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public float speed = 1;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //rb.velocity = transform.right * -speed;
+        rb.AddForce(getRandomDirection() * speed, ForceMode2D.Impulse);
+    }
+
+    private Vector2 getRandomDirection()
+    {
+        Vector2 randomDirection = Random.insideUnitCircle;
+        randomDirection.Normalize();
+        randomDirection.x = -Mathf.Abs(randomDirection.x);
+        return randomDirection;
+    }
+
+}
---
+++ 2D_UFO/Assets/Scripts/SpawnController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] hazardPrefabs;
+    [SerializeField]
+    private Vector3 spawnValues;
+    [SerializeField]
+    private int hazardCount;
+    [SerializeField]
+    private float spawnWait, startWait, waveWait;
+	
+    private void Start()
+    {
+        StartCoroutine(SpawnWaves());
+    }
+
+	private IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazardPrefabs[Random.Range(0, hazardPrefabs.Length)];
+                Vector3 spawnPosition = new Vector3(spawnValues.x, Random.Range(-spawnValues.y, spawnValues.y), spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+}

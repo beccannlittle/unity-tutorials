ccc9ff8db6c0ad72bf71828fcb177df7f7939905
Becca Little <beccannlittle@gmail.com>
2018-09-06 18:52:59 -0700

    finished AdventureGame

---
+++ Adventure_Game_5/Assets/Scripts/Editor/Interaction/InteractableEditor.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+[CustomEditor(typeof(Interactable))]
+public class InteractableEditor : EditorWithSubEditors<ConditionCollectionEditor, ConditionCollection> {
+
+    private SerializedProperty interactionLocationProperty;
+    private SerializedProperty collectionsProperty;
+    private SerializedProperty defaultReactionCollectionProperty;
+
+    private Interactable interactable;
+
+    private const string interactablePropInteractionLocationName = "interactionLocation";
+    private const string interactablePropConditionCollectionsName = "conditionCollections";
+    private const string interactablePropDefaultReactionCollectionName = "defaultReactionCollection";
+
+    private const float collectionButtonWidth = 125f;
+
+    private void OnEnable()
+    {
+        interactionLocationProperty = serializedObject.FindProperty(interactablePropInteractionLocationName);
+        collectionsProperty = serializedObject.FindProperty(interactablePropConditionCollectionsName);
+        defaultReactionCollectionProperty = serializedObject.FindProperty(interactablePropDefaultReactionCollectionName);
+
+        interactable = (Interactable)target;
+
+        CheckAndCreateSubEditors(interactable.conditionCollections);
+    }
+
+    private void OnDisable()
+    {
+        CleanupEditors();
+    }
+
+    protected override void SubEditorSetup(ConditionCollectionEditor editor)
+    {
+        editor.collectionsProperty = collectionsProperty;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        CheckAndCreateSubEditors(interactable.conditionCollections);
+
+        EditorGUILayout.PropertyField(interactionLocationProperty);
+
+        for (int i = 0; i < subEditors.Length; i++)
+        {
+            subEditors[i].OnInspectorGUI();
+            EditorGUILayout.Space();
+        }
+
+        EditorGUILayout.BeginHorizontal();
+
+        GUILayout.FlexibleSpace();
+        if (GUILayout.Button("Add Collection", GUILayout.Width(collectionButtonWidth)))
+        {
+            ConditionCollection newCollection = ConditionCollectionEditor.CreateConditionCollection();
+            collectionsProperty.AddToObjectArray(newCollection);
+        }
+
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.PropertyField(defaultReactionCollectionProperty);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
